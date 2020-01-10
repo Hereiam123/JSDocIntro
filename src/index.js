ea7d2573f70a9b8834d5c6c6bdbeacc5cc4a5d9f
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-10 13:32:59 -0500

    Checking types

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//@ts-check
+
+/**
+ * Student Name
+ * @type {string}
+ */
+const studentName = "John Doe";
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [98, 97.7, 76, 89];
+
+/**
+ * @type {{id: number, text: string}}
+ */
+const todo = {
+  id: 1,
+  text: "Hello"
+};
